6f29f051aa5bd21cae6d32411a5e999b8c1b19fa
Thomas Sloboda <thomas@sloboda.fr>
2023-09-27 08:38:45 +0200

    feat(tracker): add basic tracker

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ document.addEventListener("readystatechange", function (e) {
     document.querySelectorAll("img[data-lazysrc]").forEach(function (img) {
       img.setAttribute("src", img.getAttribute("data-lazysrc"));
     });
+    const referrer = document.referrer;
+    const fromParameter = new URLSearchParams(window.location.href).get("from");
+    fetch("https://personaltracker-1-g7781331.deta.app/", {
+      method: "POST",
+      body: JSON.stringify({ from: fromParameter ?? referrer }),
+    });
   }
 });
